67cb75e79f7d500eecfd1fe5833cbd51c3fce316
keeks-mtl <66472161+keeks-mtl@users.noreply.github.com>
2023-09-17 16:36:08 +0100

    create routes for pages on app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
+import Play from "./Components/Play";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/play" element={<Play />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
