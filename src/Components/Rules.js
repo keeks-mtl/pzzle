984c230aa74258a74474a2ea0c4caed7a744ab3c
keeks-mtl <wallarand@gmail.com>
2023-09-18 17:24:53 +0100

    fix responsiveness of rules page

--- src/Components/Rules.js
+++ src/Components/Rules.js
@@ -10,68 +10,76 @@ const Rules = () => {
   return (
     <>
     <Grid container mt={8}>
-      <Grid item lg={3}>
+      <Grid item xs={12} md={6} lg={3}>
         <Box
+        className="rules-box"
           mx={6}
+          my={4}
           sx={{
-            border: "1px dashed grey",
             height: "580px",
             backgroundColor: "#fbfbfb",
             borderRadius: "5%",
+            display:"grid"
           }}
         >
-          <img src={lightbulb} alt="illustration of a lightbulb where part of it is a puzzle piece" style={{ height: "300px" }} />
+          <img src={lightbulb} alt="illustration of a lightbulb where part of it is a puzzle piece" style={{ height: "250px", justifySelf:"center", marginTop:"30px" }} />
           <Typography variant="h4" p={4}> Look at the puzzle and think of how you might imagine the puzzle to fit</Typography>
         </Box>
       </Grid>
-      <Grid item lg={3}>
+      <Grid item xs={12} md={6} lg={3}>
         <Box
           Box
           mx={6}
+          my={4}
           sx={{
-            border: "1px dashed grey",
             height: "580px",
             backgroundColor: "#fbfbfb",
             borderRadius: "5%",
+            display:"grid"
           }}
+          className="rules-box"
         >
-          <img src={moveImg} alt="illustration of two puzzle pieces and an arrow showing movement" style={{ height: "300px" }} />
+          <img src={moveImg} alt="illustration of two puzzle pieces and an arrow showing movement" style={{ height: "250px", justifySelf:"center", marginTop:"30px" }} />
           <Typography variant="h4" p={4}>Move the puzzle pieces around to try to find where they belong</Typography>
         </Box>
       </Grid>
-      <Grid item lg={3}>
+      <Grid item xs={12} md={6} lg={3}>
         <Box
           Box
           mx={6}
+          my={4}
           sx={{
-            border: "1px dashed grey",
             height: "580px",
             backgroundColor: "#fbfbfb",
             borderRadius: "5%",
+            display:"grid"
           }}
+          className="rules-box"
         >
-         <img src={workImg} alt="illustration of people working on a puzzle" style={{ height: "300px" }} />
-         <Typography variant="h4" p={4}> Once a puzzle piece is in the correct position it will lock into place and can no longer be moved</Typography>
+         <img src={workImg} alt="illustration of people working on a puzzle" style={{ height: "250px", justifySelf:"center", marginTop:"30px" }} />
+         <Typography variant="h4" p={4}> When in the correct position, the puzzle piece can no longer be moved</Typography>
         </Box>
       </Grid>
-      <Grid item lg={3}>
+      <Grid item xs={12} md={6} lg={3}>
         <Box
           Box
           mx={6}
+          my={4}
           sx={{
-            border: "1px dashed grey",
             height: "580px",
             backgroundColor: "#fbfbfb",
             borderRadius: "5%",
+            display:"grid"
           }}
+          className="rules-box"
         >
-          <img src={fitPuzzle} alt="picture of puzzle pieces fitting together" style={{ height: "300px" }} />
+          <img src={fitPuzzle} alt="picture of puzzle pieces fitting together" style={{ height: "250px", justifySelf:"center", marginTop:"30px" }} />
           <Typography variant="h4" p={4}>Try to find the correct place for all the puzzle pieces to finish the puzzle</Typography>
         </Box>
       </Grid>
     </Grid>
-    <Grid container mt={20}>
-        <Grid item lg={12}>
+    <Grid container mt={20} justifyContent="center">
+        <Grid item lg={2} >
             <Link to="/play"> <Button variant="contained"
                 disableElevation 
                 sx={{ backgroundColor: "#ea5d2d", width:"300px" }}> Play Now </Button> </Link>
