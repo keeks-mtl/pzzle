d8d61369e92ef23169b6a4a0d2f36117d5b28301
keeks-mtl <wallarand@gmail.com>
2023-09-18 18:02:08 +0100

    make play page responsive

--- src/Components/Play.js
+++ src/Components/Play.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { Grid, Box, Typography, Stack, Button } from "@mui/material";
 import ExtensionIcon from "@mui/icons-material/Extension";
@@ -6,25 +6,34 @@ import { JigsawPuzzle } from "react-jigsaw-puzzle/lib";
 import "react-jigsaw-puzzle/lib/jigsaw-puzzle.css";
 
 const Play = () => {
-  const [message, setMessage] = useState("Solve the pzzle!");
-
-  const solve = () => {
-    setMessage("Congratulations!!");
-  };
-
   return (
     <div>
-      <Grid container mt={8}>
-        <Grid item lg={3}>
+      <Grid container mt={8} justifyContent="center">
+        <Grid item xs={12} sm={10} md={6} lg={6}>
+          <Box className="jigsaw-box">
+            <JigsawPuzzle
+              imageSrc="https://static.vecteezy.com/system/resources/previews/005/018/282/non_2x/colored-puzzle-pieces-in-series-vector.jpg"
+              rows={3}
+              columns={4}
+            />
+          </Box>
+        </Grid>
+        <Grid item xs={10} sm={10} md={4} lg={3}>
           <Box
             sx={{
-              height: "620px",
-              backgroundColor: "#fbfbfb",
+              backgroundColor: "#fbf8f6",
               borderRadius: "5%",
-              marginLeft: "2rem",
+              display: "grid",
             }}
           >
-            <ExtensionIcon sx={{ fontSize: "15rem", color: "#ea5d2d" }} />
+            <ExtensionIcon
+              sx={{
+                fontSize: "15rem",
+                color: "#ea5d2d",
+                justifySelf: "center",
+                paddingTop: "10px",
+              }}
+            />
             <Typography variant="h2" sx={{ textAlign: "center" }}>
               To Play:
             </Typography>
@@ -33,57 +42,28 @@ const Play = () => {
               px={4}
               sx={{ textAlign: "left", fontSize: "1rem" }}
             >
-              Move the puzzle pieces to complete the image. Once a puzzle piece
-              is in the correct position it is locked and can no longer be
-              moved.
+              Move the puzzle pieces to complete the image.
             </Typography>
             <Typography
               variant="subtitle1"
               pl={4}
-              pt={2}
+              py={2}
               sx={{ textAlign: "left", fontSize: "1rem" }}
             >
               Best of luck with solving!
             </Typography>
-            <Link to="/rules">
-            <Button variant="contained"
+            <Link to="/rules" style={{ justifySelf: "center" }}>
+              <Button
+                variant="contained"
                 disableElevation
-                sx={{ backgroundColor: "#ea5d2d" }}> Rules </Button>
+                sx={{ backgroundColor: "#ea5d2d", marginBottom: "30px" }}
+              >
+                {" "}
+                Rules{" "}
+              </Button>
             </Link>
-            
           </Box>
         </Grid>
-        <Grid item lg={6}>
-          {/* <Box pt={6} sx={{ border: "1px dashed grey", height: "580px" }}> */}
-          <Box
-            pt={6}
-            sx={{
-              height: "580px",
-            }}
-          >
-            <JigsawPuzzle
-              imageSrc="https://static.vecteezy.com/system/resources/previews/005/018/282/non_2x/colored-puzzle-pieces-in-series-vector.jpg"
-              rows={3}
-              columns={4}
-              onSolved={solve}
-            />
-          </Box>
-        </Grid>
-        {/* <Grid item lg={3}>
-          <Box
-            className="message-box"
-            sx={{
-                backgroundColor: "#fbfbfb",
-              borderRadius: "5%",
-              height: "620px",
-              marginRight: "2rem",
-            }}
-          >
-            <Typography className="message-text" sx={{ fontSize: "3rem" }}>
-              {message}
-            </Typography>
-          </Box>
-        </Grid> */}
       </Grid>
       ;
     </div>
