85c0c79af8f913931735b7acc321acb467d094f6
keeks-mtl <wallarand@gmail.com>
2023-09-19 13:34:50 +0100

    add comments to contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { Grid, Box, TextField, Typography, FormControl, Button } from "@mui/material";
+import {
+  Grid,
+  Box,
+  TextField,
+  Typography,
+  FormControl,
+  Button,
+} from "@mui/material";
 import puzzleQ from "../Assets/puzzle-question.png";
-
 const Contact = () => {
   return (
     <>
+      {/* Contact Form and Image Grid */}
       <Grid container justifyContent="center" mt={8}>
+        {/* Contact Form */}
         <Grid item xs={12} sm={6} lg={5} xl={3}>
           <Box
             p={5}
@@ -20,24 +28,27 @@ const Contact = () => {
             <Typography variant="subtitle2" sx={{ mt: 2, mb: 2 }}>
               We're here to help! Send us your query via the form below or send
               us an email at{" "}
-              <a href="mailto:helpdesk@pzzle.com" className="orange-link">helpdesk@pzzle.com</a> for any
-              issue your're facing
+              <a href="mailto:helpdesk@pzzle.com" className="orange-link">
+                helpdesk@pzzle.com
+              </a>{" "}
+              for any issue you're facing
             </Typography>
-            <FormControl sx={{width: "85%"}}>
+            {/* Contact Form Fields */}
+            <FormControl sx={{ width: "85%" }}>
               <TextField
                 required
                 id="name"
                 label="Name"
                 variant="filled"
                 margin="normal"
-              ></TextField>
+              />
               <TextField
                 required
                 id="email"
                 label="Email"
                 variant="filled"
                 margin="normal"
-              ></TextField>
+              />
               <TextField
                 required
                 fullWidth
@@ -48,27 +59,33 @@ const Contact = () => {
                 multiline
                 rows={5}
                 maxRows={5}
-                sx={{  resize: "none" }}
-              ></TextField>
-              <Button    variant="contained"
+                sx={{ resize: "none" }}
+              />
+              {/* Submit Button */}
+              <Button
+                variant="contained"
                 disableElevation
-                sx={{ backgroundColor: "#ea5d2d" }}> Submit </Button>
+                sx={{ backgroundColor: "#ea5d2d" }}
+              >
+                Submit
+              </Button>
             </FormControl>
           </Box>
         </Grid>
+        {/* Contact Image */}
         <Grid item xs={12} sm={6} lg={5} xl={3}>
           <Box
-          className="contact-image-box"
+            className="contact-image-box"
             sx={{
               height: "620px",
               backgroundColor: "#ea5d2d",
-              display:"flex"
+              display: "flex",
             }}
           >
             <img
               src={puzzleQ}
               alt="illustration of people working on a puzzle"
-              style={{ height: "50%", margin: "auto", position:"relative" }}
+              style={{ height: "50%", margin: "auto", position: "relative" }}
             />
           </Box>
         </Grid>
@@ -76,5 +93,4 @@ const Contact = () => {
     </>
   );
 };
-
 export default Contact;
